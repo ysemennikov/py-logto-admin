656f8be856d72b2c7316c6d355c0f9e705d2dfb6
Yaroslav Semennikov <52397003+ysemennikov@users.noreply.github.com>
2024-06-17 11:51:31 +0200

    refactor: replace jinja2 template with built-in openapi-python-client templates (#4)

--- dev.py
+++ dev.py
@@ -1,20 +1,18 @@
+from pathlib import Path
 from shutil import copyfile
 from subprocess import run as run_command
+from tempfile import TemporaryDirectory
 
-from jinja2 import Environment, FileSystemLoader
+import httpx
 from typer import Typer
 
 app = Typer()
-j2_env = Environment(loader=FileSystemLoader("./src"))
 
 
 @app.command()
 def copy_from_src():
     """Copy updated files from src to logto_admin."""
     copyfile("./src/token_manager.py", "./logto_admin/token_manager.py")
-
-    with open("./logto_admin/__init__.py", "w", encoding="utf-8") as f:
-        f.write(j2_env.get_template("__init__.jinja").render())
 
 
 @app.command()
@@ -25,20 +23,35 @@ def generate(url: str):
     This will overwrite the existing client.
     """
 
-    run_command(
-        [
-            "openapi-python-client",
-            "generate",
-            "--meta=none",
-            "--output-path=logto_admin",
-            "--overwrite",
-            "--url",
-            url,
-            "--config",
-            "./src/config.yaml",
-        ],
-        check=False,
-    )
+    # fetch the openapi spec
+    openapi_resp = httpx.get(url)
+    openapi_resp.raise_for_status()
+
+    with TemporaryDirectory() as temp_dir_name:
+        temp_dir = Path(temp_dir_name)
+        openapi_temp_path = temp_dir / "openapi.json"
+
+        # save the openapi spec
+        with open(openapi_temp_path, "wb") as f:
+            f.write(openapi_resp.content)
+
+        # generate the client
+        run_command(
+            [
+                "openapi-python-client",
+                "generate",
+                "--meta=none",
+                "--output-path=logto_admin",
+                "--overwrite",
+                "--path",
+                str(openapi_temp_path),
+                "--config",
+                "./src/config.yaml",
+                "--custom-template-path=./src/templates",
+            ],
+            check=False,
+        )
+
     with open("./logto_admin/py.typed", "w", encoding="utf-8") as f:
         f.write("# Marker file for PEP 561")
 
